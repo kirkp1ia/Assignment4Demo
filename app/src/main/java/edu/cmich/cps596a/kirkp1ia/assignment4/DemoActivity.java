80927d30676a1e1f53d33c698f8ddb666717543f
kirkp1ia <kirkp1ia@aux-vm-bb9a98.central.cmich.local>
2016-11-28 11:28:47 -0500

    finish demo project

--- app/src/main/java/edu/cmich/cps596a/kirkp1ia/assignment4/DemoActivity.java
+++ app/src/main/java/edu/cmich/cps596a/kirkp1ia/assignment4/DemoActivity.java
@@ -1,14 +1,14 @@
 package edu.cmich.cps596a.kirkp1ia.assignment4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import org.json.simple.parser.ParseException;
+import java.util.List;
 
-import java.io.FileNotFoundException;
-import java.sql.SQLException;
-
-import kirkModels.orm.Project;
+import edu.cmich.cps596a.kirkp1ia.assignment4.demos.Person;
 
 public class DemoActivity extends AppCompatActivity {
 
@@ -16,15 +16,25 @@ public class DemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+    }
 
-//        try {
-//            Project.initialize("settings/settings.json");
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+    public void getAllPeople(View v) {
+        List<Person> people = Person.listAll(Person.class);
+        String strPeople = "";
+        for (Person p : people) {
+            strPeople = strPeople + "\n" + p.toString();
+        }
+
+        TextView peopleView = (TextView) this.findViewById(R.id.view_people);
+        peopleView.setText(strPeople);
+    }
+
+    public void newPersonClicked(View v) {
+        String name = ((EditText) this.findViewById(R.id.input__name)).getText().toString();
+        String nickName = ((EditText) this.findViewById(R.id.input__nickname)).getText().toString();
+        int age = Integer.valueOf(((EditText) this.findViewById(R.id.input__age)).getText().toString());
+
+        Person p = new Person(name, nickName, age);
+        p.save();
     }
 }
---
+++ app/src/main/java/edu/cmich/cps596a/kirkp1ia/assignment4/demos/Person.java
@@ -0,0 +1,28 @@
+package edu.cmich.cps596a.kirkp1ia.assignment4.demos;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by kirkp1ia on 11/28/16.
+ */
+
+public class Person extends SugarRecord {
+
+    protected String name;
+    protected String nickName;
+    protected int age;
+
+    public Person() {
+
+    }
+
+    public Person(String name, String nickName, int age) {
+        this.name = name;
+        this.nickName = nickName;
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Person: " + this.name + ", age: " + this.age;
+    }
+}
